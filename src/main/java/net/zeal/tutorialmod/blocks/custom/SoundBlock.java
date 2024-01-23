66783dde2e4ab91edfe0b6d50cbc6d10f97dfc9f
JustinRobbins7 <robbinsju@gmail.com>
2024-01-22 19:43:31 -0600

    Tutorial ep 10 - Custom Tooltips!

--- src/main/java/net/zeal/tutorialmod/blocks/custom/SoundBlock.java
+++ src/main/java/net/zeal/tutorialmod/blocks/custom/SoundBlock.java
@@ -2,14 +2,21 @@ package net.zeal.tutorialmod.blocks.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
 
@@ -22,5 +29,12 @@ public class SoundBlock extends Block {
         // Play a sound when the player right-clicks the block
         world.playSound(player, pos, SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE.value(), SoundCategory.BLOCKS, 1f, 1f);
         return super.onUse(state, world, pos, player, hand, hit);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        // Text.literal just is text written as is. Generally worse than Text.translatable.
+        tooltip.add(Text.literal("Plays sound when right-clicked."));
+        super.appendTooltip(stack, world, tooltip, options);
     }
 }
--- src/main/java/net/zeal/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/zeal/tutorialmod/item/custom/MetalDetectorItem.java
@@ -3,12 +3,18 @@ package net.zeal.tutorialmod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
 
@@ -30,7 +36,7 @@ public class MetalDetectorItem extends Item {
                 BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
 
                 // If valuable block is found, inform the player and break from the loop
-                if (isValuableBlock(state)) {
+                if (isValuableBlock(state) && player != null) {
                     outputValuableCoordinates(positionClicked.down(i), player, state.getBlock());
                     foundBlock = true;
                     break;
@@ -38,20 +44,22 @@ public class MetalDetectorItem extends Item {
             }
 
             // If no block is found, inform player
-            if(!foundBlock) {
+            if(!foundBlock && player != null) {
                 player.sendMessage(Text.literal("No Valuables Found."));
             }
         }
 
         // Damage item after use
-        context.getStack().damage(1, context.getPlayer(),
-                playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+        if (context.getPlayer() != null) {
+            context.getStack().damage(1, context.getPlayer(),
+                    playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+        }
 
         return ActionResult.SUCCESS;
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
-        // Sends the palyer the coordinates of the valuable block that was found
+        // Sends the player the coordinates of the valuable block that was found
         player.sendMessage(Text.literal("Found " + block.asItem().getName().getString() + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + " , " + blockPos.getZ() + ")"), false);
     }
@@ -59,5 +67,12 @@ public class MetalDetectorItem extends Item {
     private boolean isValuableBlock(BlockState state) {
         // Return a boolean indicating if the block being evaluated is iron or diamond ore
         return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        // References an entry in the lang files to provide a translated message based on user language settings. Preferable.
+        tooltip.add(Text.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
